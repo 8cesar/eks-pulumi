df811e621e6f93f090a66bad410ea7cd0c67b8ae
Aleksanda Nikolic <Aleksandar.Nikolic@greenflux.com>
2022-03-23 14:53:36 +0000

    Merged PR 16516: Dimensions for Cosmos DB alerts
    i have added new file for dimensions (dimension is basically filter on alert, like total request > 0 and we want to match 404 from total amount). since ARM can be without dimensions and nulls are not allowed i decided to generate new ARM when there is case of dimensions.
    if dimensions are present , ARM will modified and dimensions part will be injected.
    this can work on all ARMs . I made the change only on CosmosDB for now.
    
    Related work items: #40648

--- Azure-Alerts/deploy-cosmos-db-alerts.ps1
+++ Azure-Alerts/deploy-cosmos-db-alerts.ps1
@@ -1,53 +1,84 @@
 ﻿param(
 
-[String]$JsonFile, 
-[String]$alertGroupRegr,
+    [String]$JsonFile, 
+    [String]$alertGroupRegr,
 
-$AzureResourceGroup = 'CosmoDB',
-$processname = 'parameters'
+    $AzureResourceGroup = 'CosmoDB',
+    $processname = 'parameters'
 )
-$templatefile =  '\templates\alerts\cosmosDb.json'
-$parameterfile = '\parameters\'+$JsonFile
-
+# templates and parameters
+$templatefileIncDimensions = '\templates\alerts\cosmosDbIncDimensions.json'
+$templatefile = '\templates\alerts\cosmosDb.json'
+$parameterfile = '\parameters\' + $JsonFile
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Path
 $tempFilePath = $scriptPath + "$templatefile"
-
+$tempFilePathIncDimensions = $scriptPath + "$templatefileIncDimensions"
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Path
 $paramFilePath = $scriptPath + "$parameterfile"
+$dimensionsFile = '\templates\alerts\dimensions.json'
+$dimensionsPath = $scriptPath + "$dimensionsFile"
 
-$data =@()
-
+# get the content
+$data = @()
 $data += Get-Content -Raw -path $paramFilePath  | ConvertFrom-Json 
+$deploymentName = $AzureResourceGroup #can be added date and number
+$arraydata = $data.$processname.$AzureResourceGroup
+$resourceGroup = $arraydata.resourceGroup
+$alertGroupName = $arraydata.alertGroupName
+$cosmoDBName = $arraydata.cosmoDBName
+$alertSeverity = $arraydata.alertSeverity
+$evaluationFrequency = $arraydata.evaluationFrequency
+$windowSize = $arraydata.windowSize
+$metricName = $arraydata.metricName
+$monitname = $arraydata.monitname
+$operator = $arraydata.operator
+$threshold = $arraydata.threshold
+$timeAggregation = $arraydata.timeAggregation
+$teamName = $arraydata.teamName
+$dimensionsName = $arraydata.dimensionsName
+$dimensionsOperator = $arraydata.dimensionsOperator
+$dimensionsValues = $arraydata.dimensionsValues
 
-$deploymentName=$AzureResourceGroup #can be added date and number
+for ($i = 0; $i -le $arraydata.GetUpperBound(0); $i++) {
+    # deployment
+    # check the parametars if dimensions exists
+    if ($dimensionsName[$i]) {
 
-$arraydata=$data.$processname.$AzureResourceGroup
-
-$resourceGroup=$arraydata.resourceGroup
-$alertGroupName=$arraydata.alertGroupName
-$cosmoDBName=$arraydata.cosmoDBName
-$alertSeverity=$arraydata.alertSeverity
-$evaluationFrequency=$arraydata.evaluationFrequency
-$windowSize=$arraydata.windowSize
-$metricName=$arraydata.metricName
-$monitname=$arraydata.monitname
-$operator=$arraydata.operator
-$threshold=$arraydata.threshold
-$timeAggregation=$arraydata.timeAggregation
-$teamName=$arraydata.teamName
-
-for ($i =0; $i -le $arraydata.GetUpperBound(0); $i++){
-
-
-
-$deployment = ("$deploymentName" + "$i")
-
-
-New-AzResourceGroupDeployment -name $deployment -ResourceGroupName $resourceGroup[$i] -TemplateFile $tempFilePath -alertGroupName $alertGroupName[$i] -alertGroupRegr $alertGroupRegr -cosmoDBName $cosmoDBName[$i] -alertSeverity $alertSeverity[$i] -evaluationFrequency $evaluationFrequency[$i] -windowSize  $windowSize[$i]`
--metricName $metricName[$i]`
--monitname $monitname[$i]`
--operator $operator[$i]`
--threshold $threshold[$i]`
--timeAggregation $timeAggregation[$i]`
--teamName $teamName[$i] -Verbose
+        # json converter changes the type and [] will be missing once dimensions json is injected which is mandatory for ARM temaplate.
+        [System.Collections.ArrayList]$dimensions = Get-Content $dimensionsPath | ConvertFrom-Json
+    
+        # create a list and then remove one member will do the trick
+        $dimensions.RemoveAt(1)
+    
+        # change the properties
+        $tempFile = Get-Content $tempFilePath | ConvertFrom-Json   
+        $tempFile.resources.properties.criteria.allOf | Add-Member -MemberType NoteProperty -Name dimensions -Value $dimensions -Force
+        $tempFile.resources.properties.criteria.allOf.dimensions[0].name = $dimensionsName[$i]
+        $tempFile.resources.properties.criteria.allOf.dimensions[0].operator = $dimensionsOperator[$i]
+        $tempFile.resources.properties.criteria.allOf.dimensions[0].values.SetValue($dimensionsValues[$i], 0)
+    
+        #convert to json and clear /u0022 caracters
+        $newArm = $tempFile | ConvertTo-Json -Depth 100 | % { [System.Text.RegularExpressions.Regex]::Unescape($_) }
+        #phizical file is mandarory
+        $newArm | Out-File -FilePath $tempFilePathIncDimensions
+        # deployment
+        $deployment = ("$deploymentName" + "dimensions" + "$i")
+        New-AzResourceGroupDeployment -name $deployment -ResourceGroupName $resourceGroup[$i] -TemplateFile $tempFilePathIncDimensions -alertGroupName $alertGroupName[$i] -alertGroupRegr $alertGroupRegr -cosmoDBName $cosmoDBName[$i] -alertSeverity $alertSeverity[$i] -evaluationFrequency $evaluationFrequency[$i] -windowSize  $windowSize[$i]`
+            -metricName $metricName[$i]`
+            -monitname $monitname[$i]`
+            -operator $operator[$i]`
+            -threshold $threshold[$i]`
+            -timeAggregation $timeAggregation[$i]`
+            -teamName $teamName[$i] -Verbose     
+    }
+    else {
+        $deployment = ("$deploymentName" + "$i")
+        New-AzResourceGroupDeployment -name $deployment -ResourceGroupName $resourceGroup[$i] -TemplateFile $tempFilePath -alertGroupName $alertGroupName[$i] -alertGroupRegr $alertGroupRegr -cosmoDBName $cosmoDBName[$i] -alertSeverity $alertSeverity[$i] -evaluationFrequency $evaluationFrequency[$i] -windowSize  $windowSize[$i]`
+            -metricName $metricName[$i]`
+            -monitname $monitname[$i]`
+            -operator $operator[$i]`
+            -threshold $threshold[$i]`
+            -timeAggregation $timeAggregation[$i]`
+            -teamName $teamName[$i] -Verbose
+    }
 }
